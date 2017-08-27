ec0f8f3a321710ea0ad906508d56d8761fea1025
Arc <garrrus@yandex.ru>
2017-08-27 12:33:25 +0300

    started a new project asp.net.mvc.4.5

---
+++ TestWebAppMvc/TestWebAppMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestWebAppMvc.MvcApplication" Language="C#" %>
---
+++ TestWebAppMvc/TestWebAppMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebAppMvc.Startup))]
+namespace TestWebAppMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
