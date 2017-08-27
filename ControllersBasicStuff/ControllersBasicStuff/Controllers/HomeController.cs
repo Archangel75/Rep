cfc75ba0b8929f0ce8e52ff6ab89b558330f0049
Arc <garrrus@yandex.ru>
2017-08-27 15:36:13 +0300

    Added method GetCar with get and post stuff. added methods to

--- ControllersBasicStuff/ControllersBasicStuff/Controllers/HomeController.cs
+++ ControllersBasicStuff/ControllersBasicStuff/Controllers/HomeController.cs
@@ -13,6 +13,32 @@ namespace ControllersBasicStuff.Controllers
             return View();
         }
 
+        public string GetId(int id)
+        {
+
+            return id.ToString();
+        }
+
+        [HttpGet]        
+        public ActionResult GetCar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string GetCar(string title, string model)
+        {
+
+            return title + " " + model;
+        }
+
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int b = Int32.Parse(Request.Params["b"]);
+            return "Площадь треугольника = " + (a*b/2).ToString();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
