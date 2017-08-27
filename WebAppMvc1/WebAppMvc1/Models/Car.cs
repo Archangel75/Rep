6bc857ca9f87779ce15bfa443e769c3be7d147d9
Arc <garrrus@yandex.ru>
2017-08-27 13:01:40 +0300

    added Car.cs in models

---
+++ WebAppMvc1/WebAppMvc1/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMvc1.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public int Price { get; set; }
+    }
+}
