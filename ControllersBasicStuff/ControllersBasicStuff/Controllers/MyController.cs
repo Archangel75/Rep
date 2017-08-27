0ac7b381ab21e012ec35b6780252ef3c20eef8d0
Arc <garrrus@yandex.ru>
2017-08-27 15:16:02 +0300

    new project for leeson8

---
+++ ControllersBasicStuff/ControllersBasicStuff/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersBasicStuff.Controllers
+{
+    public class MyController : IController
+    {
+        public void Execute(System.Web.Routing.RequestContext requestContext)
+        {
+            string ip = requestContext.HttpContext.Request.UserHostAddress;
+            var responce = requestContext.HttpContext.Response;
+            responce.Write("<h2>Ваш ip: " + ip + "</h2>");            
+        }
+    }
+}
