c98bbcedd220ba868901146e34a217d576c00e18
Arc <garrrus@yandex.ru>
2017-08-28 13:41:18 +0300

    Added examples how to work with async methods. Added the mvc default automaticly generated async class BooksController. Remembered how to work with table and foreach stuff in views.

--- AsyncTaskApp/AsyncTaskApp/Models/BookContext.cs
+++ AsyncTaskApp/AsyncTaskApp/Models/BookContext.cs
@@ -10,4 +10,17 @@ namespace AsyncTaskApp.Models
     {
         public DbSet<Book> Books { get; set; }
     }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { name = "Moseratti", author = "A2 2015", price = 100000 });
+            context.Books.Add(new Book { name = "Жигули", author = "Веста", price = 1000 });
+            context.Books.Add(new Book { name = "Ford", author = "Focus", price = 5000 });
+
+            base.Seed(context);
+        }
+
+    }
 }
