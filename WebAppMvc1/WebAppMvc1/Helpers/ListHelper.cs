975fbc6ae77cc25520e5145b0f89deb15e13c8a5
Arc <garrrus@yandex.ru>
2017-08-29 14:29:53 +0300

    Some issue with copy packages appears. Added Helpers folder with Helpers class which provides a way to work with helpers. added helper into index.cshtml

---
+++ WebAppMvc1/WebAppMvc1/Helpers/ListHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarAppStore.Helpers
+{
+    public static class ListHelper
+    {
+        public static MvcHtmlString CreateList(this HtmlHelper html, string[] items)
+        {
+            //тэг списка
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                //тэг элемента списка
+                TagBuilder li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ul.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ul.ToString());
+        }
+    }
+}
